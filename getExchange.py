fb53765aa7576cede43080a275eda7c85a0180c6
Thomas Kjøglum <tkjoeglu@gmail.com>
2020-02-04 20:42:49 +0100

    Removed an unused class

--- getExchange.py
+++ getExchange.py
@@ -27,30 +27,6 @@ class Exchange:
         r = requests.get(url)
         return r.json()
 
-class Graph:
-    """Graphing of stocks and exchange rates."""
-
-    def stock(self, df):
-        df = pd.DataFrame(df)
-        df["date"] = pd.to_datetime(df["date"])
-
-        inc = df.close > df.open
-        dec = df.open > df.close
-        w = 12*60*60*1000 # half day in ms
-
-        TOOLS = "pan,wheel_zoom,box_zoom,reset,save"
-
-        p = figure(x_axis_type="datetime", tools=TOOLS, plot_width=1000, title = "MSFT Candlestick")
-        p.xaxis.major_label_orientation = pi/4
-        p.grid.grid_line_alpha=0.3
-
-        p.segment(df.date, df.high, df.date, df.low, color="black")
-        p.vbar(df.date[inc], w, df.open[inc], df.close[inc], fill_color="#D5E1DD", line_color="black")
-        p.vbar(df.date[dec], w, df.open[dec], df.close[dec], fill_color="#F2583E", line_color="black")
-
-        output_file("candlestick.html", title="candlestick.py example")
-
-        show(p)  # open a browser
 
 if __name__ == '__main__':
     e = Exchange()
